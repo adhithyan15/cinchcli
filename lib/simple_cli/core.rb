f998026e0069ff6881615cba68e68b410620d85c
Adhithya Rajasekaran <adhithyan15@gmail.com>
2015-06-13 18:54:11 -0700

    For some weird reason, Ruby is complaining that SimpleCli is not a class. So I am changing it to SimpleCliBuilder

--- lib/simple_cli/core.rb
+++ lib/simple_cli/core.rb
@@ -1,6 +1,6 @@
 require 'json'
 
-class SimpleCli
+class SimpleCliBuilder
   def initialize(config_file)
     @allowable_arguments = config_file
   end
@@ -12,6 +12,6 @@ class SimpleCli
   end
 
   def verify_config_file
-    
+
   end
 end
